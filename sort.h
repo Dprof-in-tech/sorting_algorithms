a6da139e9f0712dcea80606648b05628f5b59cac
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-01-24 00:55:16 +0000

    task 3 done

--- sort.h
+++ sort.h
@@ -27,5 +27,8 @@ void swap_node(listint_t **head, listint_t **node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 
 #endif
